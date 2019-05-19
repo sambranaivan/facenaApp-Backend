7a927d4479242cabb996f2b073964f8f44d20448
Sambrana Ivan <sambranaivan@gmail.com>
2019-05-19 16:45:16 -0300

    opt

--- app/Http/Controllers/mapucheController.php
+++ app/Http/Controllers/mapucheController.php
@@ -8,7 +8,7 @@ class mapucheController extends Controller
 {
     //
 
-    public function index(){
+    public function indexx(){
         $ch = curl_init();
         $url = "https://10.20.15.80/mapuche/agentes/legajo/4028";
         curl_setopt($ch, CURLOPT_URL, $url);
@@ -24,5 +24,38 @@ class mapucheController extends Controller
         echo"</br>";
 
         // print_r($output);
+    }
+
+    public function index(){
+            //The URL of the resource that is protected by Basic HTTP Authentication.
+            $url = "https://10.20.15.80/mapuche/agentes/legajo/4028";
+
+            //Your username.
+            $username = 'exactas';
+
+            //Your password.
+            $password = 'Exa2019_';
+
+            //Initiate cURL.
+            $ch = curl_init($url);
+
+            //Specify the username and password using the CURLOPT_USERPWD option.
+            curl_setopt($ch, CURLOPT_USERPWD, $username . ":" . $password);
+            curl_setopt($ch, CURLOPT_PORT, 7070);
+            //Tell cURL to return the output as a string instead
+            //of dumping it to the browser.
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+            //Execute the cURL request.
+            $response = curl_exec($ch);
+
+            //Check for errors.
+            if(curl_errno($ch)){
+            //If an error occured, throw an Exception.
+            throw new Exception(curl_error($ch));
+            }
+
+            //Print out the response.
+            echo $response;
     }
 }
