1a6893dde387f872583a11db1cdf9fe83aa7f047
Sambrana Ivan <sambranaivan@gmail.com>
2019-05-19 17:26:13 -0300

    curd

--- app/Http/Controllers/mapucheController.php
+++ app/Http/Controllers/mapucheController.php
@@ -68,7 +68,7 @@ class mapucheController extends Controller
 
                     $client = new Client(['proxy' => 'http://proxyroca:3128/']);
                     $res = $client->request('GET', "https://10.20.15.80:7070*/mapuche/agentes/legajo/4028", [
-                    'auth' => ['exactas', 'Exa2019_']
+                    'auth' => ['mapuche', '123456']
                     ]);
                     echo $res->getStatusCode();
                     // "200"
